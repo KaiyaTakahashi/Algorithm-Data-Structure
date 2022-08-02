8214bf6afea2ab302717f92adc54d0713802236d
Kaiya Takahashi <yaikashi@icloud.com>
2022-08-02 15:27:30 -0700

    finish findCenterOfStarGraph

--- Swift-AGDS/Graph/FindCenterOfStarGraph.swift
+++ Swift-AGDS/Graph/FindCenterOfStarGraph.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+func findCenter(_ edges: [[Int]]) -> Int {
+    var commonV = -1
+    if edges[0].contains(edges[1][1]) {
+        commonV = edges[1][1]
+    } else {
+        commonV = edges[1][0]
+    }
+    return commonV
+}
